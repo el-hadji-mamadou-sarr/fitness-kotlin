c5b6327b8f7bdc3d0a34c92d6e4f731f7617504a
gloatingorc <gloatingorc@gmail.com>
2023-05-29 11:37:28 +0200

    first commit

---
+++ app/src/main/java/com/example/fitshare/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.fitshare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    private lateinit var auth: FirebaseAuth // Declare Firebase Auth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        emailEditText = findViewById(R.id.email)
+        passwordEditText = findViewById(R.id.password)
+        loginButton = findViewById(R.id.login)
+
+        auth = FirebaseAuth.getInstance() // Initialize Firebase Auth
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Check if user entered all fields
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                // Sign in user using Firebase Auth
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            // Redirect user to home screen
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Please enter all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
+}
